32da2c7737eb2d5983f5ca9444d6c301fd30b4ac
최영운 <ywchoi@catis.biz>
2022-05-10 14:55:14 +0900

    7

--- Sources/MyLib220510/File 2.swift
+++ Sources/MyLib220510/File 2.swift
@@ -21,3 +21,40 @@ struct Point {
         }
     }
 }
+public struct SomeType{
+    private var count: Int = 0
+    public var publicStoredProperty: Int = 0
+    public private(set) var publicGetOnlyStoredPropety: Int = 0
+    internal var internalComputedProperty: Int{
+        get{
+            return count
+        }
+        set{
+            count += 1
+        }
+    }
+    internal private(set) var internalGetOnlyComputedProperty: Int{
+        get{
+            return count
+        }
+        set{
+            count += 1
+        }
+    }
+    public subscript()-> Int{
+        get{
+            return count
+        }
+        set{
+            count += 1
+        }
+    }
+    public internal(set) subscript(some: Int) -> Int{
+        get{
+            return count
+        }
+        set{
+            count += 1
+        }
+    }
+}
